7a56d0564512db9e94d9ad2aad9fa72e1be4a44c
Punish_Pratap <156414861+Punish2434@users.noreply.github.com>
2024-05-09 20:51:28 +0530

    Create Divisor Game.java

---
+++ GFG/Divisor Game.java
@@ -0,0 +1,51 @@
+/*     Scroll below to see JAVA code and C# also    */
+/*
+   
+    Company Tags                : Amazon
+    GFG  Link               : https://www.geeksforgeeks.org/problems/divisor-game-1664432414/1
+*/
+
+
+/ C++ ****************************************************************************************************************************************/
+
+public class Solution
+{
+    bool divisorGame(int n) {
+        // code here
+        return n%2==0;
+    }
+}
+/ Java ****************************************************************************************************************************************/
+public class Solution
+{
+    public static boolean divisorGame(int n) {
+        // code here
+        return n%2==0;
+    }
+
+}
+// Time Complexity:- O(1)
+// Space Complexity:- O(1)
+
+/ C# ****************************************************************************************************************************************/
+
+```csharp
+using System;
+
+public class Solution
+{
+    public static bool DivisorGame(int n)
+    {
+        // code here
+        return n % 2 == 0;
+    }
+}
+```
+
+// Explanation:
+// - The `DivisorGame` method takes an integer `n` as a parameter.
+// - It checks whether `n` is even (i.e., divisible by 2) using the expression `n % 2 == 0`.
+// - If `n` is even, it returns `true`; otherwise, it returns `false`.
+
+// Feel free to ask if you have any further questions! 😊¹²³⁴
+
