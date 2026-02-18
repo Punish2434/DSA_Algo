e596a66d0e6c4ebfad1c28c499c7ea6e7973b80a
Punish_Pratap <156414861+Punish2434@users.noreply.github.com>
2026-02-18 21:18:37 +0530

    Create largest-rectangle-in-histogram.cs

---
+++ Stack/largest-rectangle-in-histogram.cs
@@ -0,0 +1,27 @@
+T C : (n) — हर element एक बार push, 
+S C :  (n) — Stack के लिए
+
+public class Solution {
+    public int LargestRectangleArea(int[] heights) {
+      int n = heights.Length;
+      Stack<int> stack = new Stack<int>();
+      int maxArea = 0;  
+      for(int i = 0; i <= n; i++)
+      {
+        int currentHeight = (i == n) ? 0 : heights[i];
+
+         while(stack.Count > 0 && heights[stack.Peek()] >= currentHeight)
+         {
+
+          int height = heights[stack.Pop()];
+
+          int width = stack.Count == 0 ? i :  i - stack.Peek() - 1;
+
+          maxArea = Math.Max(maxArea, height* width);
+         }
+         stack.Push(i);
+
+      }
+      return maxArea; 
+    }
+}
